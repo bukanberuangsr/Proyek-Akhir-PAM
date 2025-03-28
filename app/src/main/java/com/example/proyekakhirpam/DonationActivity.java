4e8b888f211b4136cacec35b5e757bb87db59b1f
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-03-28 20:26:25 +0700

    added DonationActivity as a form to replace LeftoverShareFragment

---
+++ app/src/main/java/com/example/proyekakhirpam/DonationActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyekakhirpam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DonationActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donation_activity);
+    }
+}
