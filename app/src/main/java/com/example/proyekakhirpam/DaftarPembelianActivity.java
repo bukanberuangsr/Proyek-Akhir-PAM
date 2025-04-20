6426e1bd51439ec0482d60d1d0402b5250a14f15
Kinkymaylana <kinkymaylana@gmail.com>
2025-04-20 15:33:43 +0700

    rename file dari HistoryActivity.java ke DaftarPembelianActivity.java

--- app/src/main/java/com/example/proyekakhirpam/DaftarPembelianActivity.java
+++ app/src/main/java/com/example/proyekakhirpam/DaftarPembelianActivity.java
@@ -1,21 +1,19 @@
 package com.example.proyekakhirpam;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HistoryActivity extends AppCompatActivity {
+public class DaftarPembelianActivity extends AppCompatActivity {
     private ImageView btnBack;
     private Button btnBatalkanSteak, btnBatalkanBaguette;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_daftarpembelian);
 
         // Inisialisasi komponen UI
         btnBack = findViewById(R.id.btnBack);
@@ -27,12 +25,12 @@ public class HistoryActivity extends AppCompatActivity {
 
         // Event klik tombol "Batalkan" untuk Steak
         btnBatalkanSteak.setOnClickListener(v -> {
-            Toast.makeText(HistoryActivity.this, "Pesanan Steak berhasil dibatalkan", Toast.LENGTH_SHORT).show();
+            Toast.makeText(DaftarPembelianActivity.this, "Pesanan Steak berhasil dibatalkan", Toast.LENGTH_SHORT).show();
         });
 
         // Event klik tombol "Batalkan" untuk Baguette
         btnBatalkanBaguette.setOnClickListener(v -> {
-            Toast.makeText(HistoryActivity.this, "Pesanan Baguette berhasil dibatalkan", Toast.LENGTH_SHORT).show();
+            Toast.makeText(DaftarPembelianActivity.this, "Pesanan Baguette berhasil dibatalkan", Toast.LENGTH_SHORT).show();
         });
     }
 }
