3edede9b672736f2067d23a96ab0e398da2210e5
stargazer <stargazer>
2025-06-14 16:52:03 +0700

    Refactor DonationDetail to DonationDetailActivity

--- app/src/main/java/com/example/proyekakhirpam/DonateDetailActivity.java
+++ app/src/main/java/com/example/proyekakhirpam/DonateDetailActivity.java
@@ -6,13 +6,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class DonateDetail extends AppCompatActivity {
+public class DonateDetailActivity extends AppCompatActivity {
 
     ImageView ivGambarDonasi;
     TextView tvJudulDonasi, tvDeskripsiDonasi;
