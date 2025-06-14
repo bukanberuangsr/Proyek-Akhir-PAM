0bede19ce2127e51f90d490c8f40d920738f8df1
stargazer <stargazer>
2025-06-14 17:34:09 +0700

    Update Formatting Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,17 +37,26 @@ android {
 }
 
 dependencies {
-    // Gunakan Firebase BOM dengan sintaks yang benar
+// Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
-    implementation("com.cloudinary:cloudinary-android:3.0.2")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.4")
+
+// UI & Layout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
+
+// Navigation
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
-    implementation("com.google.firebase:firebase-firestore:25.1.4")
-    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.google.android.material:material:1.12.0")
+
+// Networking
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+// Image Loading (Glide & Cloudinary)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.cloudinary:cloudinary-android:3.0.2")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
